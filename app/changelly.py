4565b8222e9d04afa9b94a8858e8e0419c119bc0
Matt Shean <mattshean@gmail.com>
2017-06-11 22:39:49 -0700

    add getMinAmount getExchangeAmount

--- app/changelly.py
+++ app/changelly.py
@@ -7,9 +7,7 @@ secrets = yaml.load(open('secrets.yaml', 'r'))
 
 baseurl = 'https://api.changelly.com'
 
-@app.route('/api/v1/secrets')
-def get_secrets():
-    return jsonify({'key': secrets['key'], 'secret': secrets['secret']})
+# endpoints
 
 @app.route('/api/v1/currencies')
 def get_currencies():
@@ -19,18 +17,59 @@ def get_currencies():
       "params": {},
       "id": 1
     }
-    headers = generate_signed_headers(payload)
+    return send_request_to_changelly(payload)
+
+@app.route('/api/v1/min/<string:from_currency>/<string:to_currency>')
+def get_min_amount(from_currency, to_currency):
+    payload = {
+      "jsonrpc": "2.0",
+      "method": "getMinAmount",
+      "params": {
+        "from": from_currency,
+        "to": to_currency,
+      },
+      "id": 1
+    }
+    return send_request_to_changelly(payload)
+
+@app.route('/api/v1/exchange/<string:from_currency>/<string:to_currency>')
+def get_exchange_rates(from_currency, to_currency, amount = 1):
+    payload = {
+      "jsonrpc": "2.0",
+      "method": "getExchangeAmount",
+      "params": {
+        "from": from_currency,
+        "to": to_currency,
+        "amount": amount
+      },
+      "id": 1
+    }
+    return send_request_to_changelly(payload)
+
+@app.route('/api/v1/exchange/<string:from_currency>/<string:to_currency>/<int:amount>')
+def get_exchange_rates_specific(from_currency, to_currency, amount = 1):
+    return get_exchange_rates(from_currency, to_currency, amount)
+
+# utlity methods
+
+def send_request_to_changelly(payload):
+    headers = create_auth_headers(payload)
     r = requests.post(baseurl, json=payload, headers=headers)
     if r.status_code == 200:
       response = json.loads(r.text)
-      return jsonify(response['result'])
-    else:
-      return jsonify(r.text)
+      if 'result' in response:
+        return jsonify(response['result'])
+      if 'error' in response:
+        return jsonify(response['error'])
+    return jsonify(r.text)
 
-def generate_signed_headers(payload):
-    sign = hmac.new(secrets['secret'], json.dumps(payload), hashlib.sha512);
+def create_auth_headers(payload):
     headers = {
       'api-key': secrets['key'],
-      'sign': sign.hexdigest()
+      'sign': generate_signed_payload(payload)
     }
     return headers
+
+def generate_signed_payload(payload):
+    sign = hmac.new(secrets['secret'], json.dumps(payload), hashlib.sha512)
+    return sign.hexdigest()
